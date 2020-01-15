ec972962f6fc10058e1ecf033234310428d48b4c
asljivo <asljivo@node0.mipvol2.ah-coap-tests-pg0.utah.cloudlab.us>
2020-01-15 07:24:06 -0700

    runsim files for different test scenarios added

---
+++ runsim10.sh
@@ -0,0 +1,26 @@
+rm results-coap/*
+rm optimization/*
+rm *.txt
+
+# m=8, 51.2, tptx=3
+
+./waf --run "test --BeaconInterval=102400 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=100 --payloadSize=64 --rho=100 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-10-4-1-102400-0-31.txt\"  --TrafficInterval=1000 --CycleTime=150000 --nControlLoops=1 --DataMode=\"MCS2_8\" --seed=10 --EnableVirtualAids=false --Nsta=50 --ProcessingDelay=5 --TxTimeMip=5000 --m=8" -j4
+
+./waf --run "test --BeaconInterval=102400 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=100 --payloadSize=64 --rho=100 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-10-4-1-102400-0-31.txt\"  --TrafficInterval=1000 --CycleTime=150000 --nControlLoops=1 --DataMode=\"MCS2_8\" --seed=9 --EnableVirtualAids=false --Nsta=50 --ProcessingDelay=5 --TxTimeMip=5000 --m=8" -j4
+
+./waf --run "test --BeaconInterval=102400 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=100 --payloadSize=64 --rho=100 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-10-4-1-102400-0-31.txt\"  --TrafficInterval=1000 --CycleTime=150000 --nControlLoops=1 --DataMode=\"MCS2_8\" --seed=8 --EnableVirtualAids=false --Nsta=50 --ProcessingDelay=5 --TxTimeMip=5000 --m=8" -j4
+
+./waf --run "test --BeaconInterval=102400 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=100 --payloadSize=64 --rho=100 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-10-4-1-102400-0-31.txt\"  --TrafficInterval=1000 --CycleTime=150000 --nControlLoops=1 --DataMode=\"MCS2_8\" --seed=7 --EnableVirtualAids=false --Nsta=50 --ProcessingDelay=5 --TxTimeMip=5000 --m=8" -j4
+
+./waf --run "test --BeaconInterval=102400 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=100 --payloadSize=64 --rho=100 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-10-4-1-102400-0-31.txt\"  --TrafficInterval=1000 --CycleTime=150000 --nControlLoops=1 --DataMode=\"MCS2_8\" --seed=6 --EnableVirtualAids=false --Nsta=50 --ProcessingDelay=5 --TxTimeMip=5000 --m=8" -j4
+
+./waf --run "test --BeaconInterval=102400 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=100 --payloadSize=64 --rho=100 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-10-4-1-102400-0-31.txt\"  --TrafficInterval=1000 --CycleTime=150000 --nControlLoops=1 --DataMode=\"MCS2_8\" --seed=5 --EnableVirtualAids=false --Nsta=50 --ProcessingDelay=5 --TxTimeMip=5000 --m=8" -j4
+
+./waf --run "test --BeaconInterval=102400 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=100 --payloadSize=64 --rho=100 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-10-4-1-102400-0-31.txt\"  --TrafficInterval=1000 --CycleTime=150000 --nControlLoops=1 --DataMode=\"MCS2_8\" --seed=4 --EnableVirtualAids=false --Nsta=50 --ProcessingDelay=5 --TxTimeMip=5000 --m=8" -j4
+
+./waf --run "test --BeaconInterval=102400 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=100 --payloadSize=64 --rho=100 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-10-4-1-102400-0-31.txt\"  --TrafficInterval=1000 --CycleTime=150000 --nControlLoops=1 --DataMode=\"MCS2_8\" --seed=3 --EnableVirtualAids=false --Nsta=50 --ProcessingDelay=5 --TxTimeMip=5000 --m=8" -j4
+
+./waf --run "test --BeaconInterval=102400 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=100 --payloadSize=64 --rho=100 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-10-4-1-102400-0-31.txt\"  --TrafficInterval=1000 --CycleTime=150000 --nControlLoops=1 --DataMode=\"MCS2_8\" --seed=2 --EnableVirtualAids=false --Nsta=50 --ProcessingDelay=5 --TxTimeMip=5000 --m=8" -j4
+
+./waf --run "test --BeaconInterval=102400 --pagePeriod=1 --pageSliceLength=1 --pageSliceCount=0 --simulationTime=100 --payloadSize=64 --rho=100 --TrafficType=\"coap\" --PrintStats=true --RAWConfigFile=\"./OptimalRawGroup/RawConfig-10-4-1-102400-0-31.txt\"  --TrafficInterval=1000 --CycleTime=150000 --nControlLoops=1 --DataMode=\"MCS2_8\" --seed=1 --EnableVirtualAids=false --Nsta=50 --ProcessingDelay=5 --TxTimeMip=5000 --m=8" -j4
+
